5008dfd07d49418a4d29aebf31561e11ac4ebdb8
drskshi <duwadiksh15@gmail.com>
2024-02-10 03:52:26 -0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SMPPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
